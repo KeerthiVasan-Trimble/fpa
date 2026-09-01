8141f3d5fff5d604730ec8baa8f3e559a2ff9883
Keerthi Vasan K <keerthivasan_kumarasamy@trimble.com>
2026-09-01 09:46:05 +0530

    PCS and RTX fix

--- 3 AgIS/03 PCS Carveout/Ag PCS Waterfall.sql
+++ 3 AgIS/03 PCS Carveout/Ag PCS Waterfall.sql
@@ -5,7 +5,31 @@ USE SCHEMA COMPUTATIONAL_COMPONENT;
 
 USE WAREHOUSE FIELD_SYSTEMS_GENERAL_WAREHOUSE;
 
-CREATE OR REPLACE VIEW FIELD_SYSTEMS_EDW.COMPUTATIONAL_COMPONENT.VIEW_AG_PCS_WATERFALL AS
+DECLARE
+    full_yr VARCHAR;
+    sql_text VARCHAR;
+BEGIN
+    SELECT FISCAL_YEAR::VARCHAR INTO :full_yr
+    FROM FIELD_SYSTEMS_EDW.ARCHITECTURAL_COMPONENT.DIMENSION_FISCAL_CALENDAR
+    WHERE CALENDAR_DATE = CURRENT_DATE();
 
-SELECT *
-FROM FIELD_SYSTEMS_EDW.BUSINESS_DATA.PCS_CARVEOUT;
+    sql_text := '
+    CREATE OR REPLACE VIEW FIELD_SYSTEMS_EDW.COMPUTATIONAL_COMPONENT.VIEW_AG_PCS_WATERFALL AS
+    SELECT
+        "GL Business Area Code",
+        "' || full_yr || '-M01" AS "' || full_yr || '-01",
+        "' || full_yr || '-M02" AS "' || full_yr || '-02",
+        "' || full_yr || '-M03" AS "' || full_yr || '-03",
+        "' || full_yr || '-M04" AS "' || full_yr || '-04",
+        "' || full_yr || '-M05" AS "' || full_yr || '-05",
+        "' || full_yr || '-M06" AS "' || full_yr || '-06",
+        "' || full_yr || '-M07" AS "' || full_yr || '-07",
+        "' || full_yr || '-M08" AS "' || full_yr || '-08",
+        "' || full_yr || '-M09" AS "' || full_yr || '-09",
+        "' || full_yr || '-M10" AS "' || full_yr || '-10",
+        "' || full_yr || '-M11" AS "' || full_yr || '-11",
+        "' || full_yr || '-M12" AS "' || full_yr || '-12"
+    FROM FIELD_SYSTEMS_EDW.BUSINESS_DATA.PCS_CARVEOUT';
+
+    EXECUTE IMMEDIATE sql_text;
+END;
--- 3 AgIS/04 RTX Carveout/Ag RTX Waterfall.sql
+++ 3 AgIS/04 RTX Carveout/Ag RTX Waterfall.sql
@@ -17,18 +17,18 @@ BEGIN
     CREATE OR REPLACE VIEW FIELD_SYSTEMS_EDW.COMPUTATIONAL_COMPONENT.VIEW_AG_RTX_WATERFALL AS
     SELECT
         "Business Area Name",
-        SUM("' || full_yr || '-M01") AS "' || full_yr || '-M01",
-        SUM("' || full_yr || '-M02") AS "' || full_yr || '-M02",
-        SUM("' || full_yr || '-M03") AS "' || full_yr || '-M03",
-        SUM("' || full_yr || '-M04") AS "' || full_yr || '-M04",
-        SUM("' || full_yr || '-M05") AS "' || full_yr || '-M05",
-        SUM("' || full_yr || '-M06") AS "' || full_yr || '-M06",
-        SUM("' || full_yr || '-M07") AS "' || full_yr || '-M07",
-        SUM("' || full_yr || '-M08") AS "' || full_yr || '-M08",
-        SUM("' || full_yr || '-M09") AS "' || full_yr || '-M09",
-        SUM("' || full_yr || '-M10") AS "' || full_yr || '-M10",
-        SUM("' || full_yr || '-M11") AS "' || full_yr || '-M11",
-        SUM("' || full_yr || '-M12") AS "' || full_yr || '-M12"
+        SUM("' || full_yr || '-M01") AS "' || full_yr || '-01",
+        SUM("' || full_yr || '-M02") AS "' || full_yr || '-02",
+        SUM("' || full_yr || '-M03") AS "' || full_yr || '-03",
+        SUM("' || full_yr || '-M04") AS "' || full_yr || '-04",
+        SUM("' || full_yr || '-M05") AS "' || full_yr || '-05",
+        SUM("' || full_yr || '-M06") AS "' || full_yr || '-06",
+        SUM("' || full_yr || '-M07") AS "' || full_yr || '-07",
+        SUM("' || full_yr || '-M08") AS "' || full_yr || '-08",
+        SUM("' || full_yr || '-M09") AS "' || full_yr || '-09",
+        SUM("' || full_yr || '-M10") AS "' || full_yr || '-10",
+        SUM("' || full_yr || '-M11") AS "' || full_yr || '-11",
+        SUM("' || full_yr || '-M12") AS "' || full_yr || '-12"
     FROM FIELD_SYSTEMS_EDW.BUSINESS_DATA.RTX_CARVEOUT
     GROUP BY "Business Area Name"';
 
